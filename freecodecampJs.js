91492c3bb1cfcda4b4ce42e30bc0f35fc6503bf4
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-05-02 21:44:45 +0400

    updateEveryDay

---
+++ freecodecampJs.js
@@ -0,0 +1,67 @@
+//https://www.freecodecamp.org/learn/front-end-libraries/react/create-a-simple-jsx-element
+
+const JSX = <h1>Hello JSX!</h1>
+
+//https://www.freecodecamp.org/learn/front-end-libraries/react/create-a-complex-jsx-element
+
+const JSX = (
+   <div>
+     <h1>JSX</h1>
+       <p>JSX</p>
+     <ul>
+       <li>JSX</li>
+       <li>JSX</li>
+       <li>JSX</li>
+     </ul>
+   </div>
+ );
+
+ //https://www.freecodecamp.org/learn/front-end-libraries/react/add-comments-in-jsx
+
+ const JSX = (
+   <div>
+     <h1>This is a block of JSX</h1>
+     <p>Here's a subtitle</p>
+     {/*comments*/}
+   </div>
+ );
+
+ //https://www.freecodecamp.org/learn/front-end-libraries/react/render-html-elements-to-the-dom
+
+ const JSX = (
+   <div>
+     <h1>Hello World</h1>
+     <p>Lets render this to the DOM</p>
+   </div>
+ );
+ ReactDOM.render(JSX,document.getElementById('challenge-node'));
+
+//https://www.freecodecamp.org/learn/front-end-libraries/react/define-an-html-class-in-jsx
+
+const JSX = (
+   <div className = "myDiv">
+     <h1>Add a class to this div</h1>
+   </div>
+ );
+
+ //https://www.freecodecamp.org/learn/front-end-libraries/react/learn-about-self-closing-jsx-tags
+
+ const JSX = (
+   <div>
+     <h2>Welcome to React!</h2> <br />
+     <p>Be sure to close all tags!</p>
+     <hr />
+   </div>
+ );
+
+ //https://www.freecodecamp.org/learn/front-end-libraries/react/create-a-stateless-functional-component
+
+const MyComponent = function() {
+   const JSX = (
+      <div>JSX</div>
+   );
+  return JSX
+}
+
+//https://www.freecodecamp.org/learn/front-end-libraries/react/create-a-react-component
+
